2fb8f4d4e0f62ab413dd3de4e8ebb1964ce796c2
Pablo Schmeiser <pablo.schmeiser2.0@gmail.com>
2025-12-17 23:23:01 +0100

    Adding day 3 p2

--- day3/solver.go
+++ day3/solver.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
+
+const NUM_DIGITS_PART2 = 12
 
 func main() {
 	numbers := parseInput()
@@ -27,7 +30,41 @@ func part1(numbers [][]int) {
 }
 
 func part2(numbers [][]int) {
-	// TODO
+	var sumMaxOutput int64 = 0
+	for _, bank := range numbers {
+		// Iterate over each digit, needed
+		for i := range NUM_DIGITS_PART2 {
+			if len(bank) == 0 {
+				break
+			}
+
+			// Only check arrays that still have enough digits left
+			highestNumPos := findHighestIndex(bank[:len(bank)-NUM_DIGITS_PART2+i+1])
+			if highestNumPos <= len(bank)-NUM_DIGITS_PART2+i {
+				// Trim the bank to only the part after the highest number found and add the number with correct place value to the sum
+				sumMaxOutput += int64(math.Pow10(NUM_DIGITS_PART2-i-1)) * int64(bank[highestNumPos])
+				bank = bank[highestNumPos+1:]
+			} else {
+				break
+			}
+
+			if i >= NUM_DIGITS_PART2-1 {
+				// If we reached the last digit empty the bank
+				bank = nil
+			}
+		}
+
+		if len(bank) == 0 {
+			continue
+		} else {
+			// If there are still numbers left, we need to add the last digits
+			for i := len(bank) - 1; i >= 0; i-- {
+				sumMaxOutput += int64(math.Pow10(len(bank)-1-i)) * int64(bank[i])
+			}
+		}
+	}
+
+	fmt.Println(sumMaxOutput)
 }
 
 func findHighestIndex(array []int) int {
